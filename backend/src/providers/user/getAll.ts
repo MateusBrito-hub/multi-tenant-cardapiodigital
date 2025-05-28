2303f890d714c1c07017a5e65ad3dff5f139a2a5
Brito <mateusvsbrito6@gmail.com>
2025-05-28 15:13:01 -0300

    fix.:  gelAll WhereCoditional

--- backend/src/providers/user/getAll.ts
+++ backend/src/providers/user/getAll.ts
@@ -1,17 +1,24 @@
-import { PrismaClient } from '../../prisma/generated/Tenant';
+import { PrismaClient, Prisma } from '../../prisma/generated/Tenant';
 
 export const getAll = async (prisma: PrismaClient, page: number, limit: number, filter: string, id = 0): Promise<object[] | Error> => {
     try {
-        const whereCondition: any = {
-            OR: [
-                id > 0 ? { id: id } : undefined,
-                filter ? { name: { contains: filter, mode: 'insensitive' } } : undefined,
-            ].filter(Boolean)
-        };
+        const whereCondition: Prisma.UserWhereInput = {};
+
+        if (id > 0) {
+            whereCondition.id = id;
+        }
+
+        if (filter) {
+            whereCondition.name = {
+                contains: filter,
+                mode: 'insensitive',
+            };
+        }
+
         const result = await prisma.user.findMany({
             where: whereCondition,
             skip: (page - 1) * limit,
-            take: limit
+            take: limit,
         });
 
         if (id > 0 && !result.some((item) => item.id === id)) {
